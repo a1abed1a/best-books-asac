a4498f283a2e2392c68d33841c5764a7526edb2f
a1abed1a <abedshehadeh1@gmail.com>
2021-09-27 21:24:36 +0300

    done

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,19 +1,57 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Jumbotron from 'react-bootstrap/Jumbotron';
+import { withAuth0 } from '@auth0/auth0-react';
+import { Jumbotron, Card, CardGroup } from 'react-bootstrap';
 import './BestBooks.css';
+const axios = require('axios');
+
 
 class MyFavoriteBooks extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    }
+  }
+
+  getData = async () => {
+    let { user } = this.props.auth0;
+    let url = `${process.env.REACT_APP_API_LINK}/myBooks?email=${user.email}`
+    let data = await axios.get(url);
+    let userData = data.data;
+    this.setState({
+      data: userData
+    })
+  }
+
   render() {
-    return(
+    this.getData()
+    return (
       <Jumbotron>
         <h1>My Favorite Books</h1>
         <p>
           This is a collection of my favorite books
         </p>
+        <CardGroup>
+          {
+            this.state.data.map((ele, idx) => {
+              return (
+                <Card>
+                  <Card.Body>
+                    <Card.Title>{ele.title}</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">{ele.status}</Card.Subtitle>
+                    <Card.Text>
+                      {ele.description}
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              );
+            })
+          }
+          </CardGroup>
       </Jumbotron>
     )
   }
 }
 
-export default MyFavoriteBooks;
+export default withAuth0(MyFavoriteBooks);
